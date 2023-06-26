4e19d2a54450ceef5ebb1fb129022543b5e5eb19
stg91723 <113889769+stg91723@users.noreply.github.com>
2023-06-26 17:06:07 +0800

    build

---
+++ build/app/generated/source/buildConfig/debug/nkustc109193202/com/classproject/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nkustc109193202.com.classproject;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "nkustc109193202.com.classproject";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
